11149744871178fcd32ce470aa533de793a6a2b0
danifick <danifick24@outlook.com>
2026-07-04 15:11:34 +0300

    Added To Do App

--- script.js
+++ script.js
@@ -327,3 +327,60 @@ for(i=0; i < acc.length; i++) {
 function moneyConverter (valNum) {
     document.getElementById("outputrub").innerHTML=valNum*77
 }
+
+let myNodeList = document.getElementsByTagName('li')
+// Строчку let i мы просто убрали к чертям, так как i уже создана выше
+
+for(i=0; i< myNodeList.length; i++) { // Тут i просто используется, всё чётко
+   let span = document.createElement("SPAN")
+   let txt = document.createTextNode("\u00D7")
+   span.className = "close"
+   span.appendChild(txt)
+   myNodeList[i].appendChild(span)
+}
+
+
+let close = document.getElementsByClassName("close")
+
+for(i=0; i< close.length; i++) {
+   close[i].onclick = function () {
+    let div =  this.parentElement
+    div.style.display = "none"
+   }
+}
+
+let list = document.querySelector('ul')
+list.addEventListener('click', function (ev) {
+    if(ev.target.tagName === 'LI') {
+       ev.target.classList.toggle('checked') 
+    }
+}, false)
+
+function newElement() {
+    let li = document.createElement('li')
+    let inputValue = document.getElementById("myInput1").value
+    let t = document.createTextNode(inputValue)
+    li.appendChild(t)
+    
+    if(inputValue === '') {
+        alert("Поле не может быть пустым!")
+    } else {
+        document.getElementById("myUL1").appendChild(li)
+
+        // Добавили создание крестика для нового таска
+        let span = document.createElement("SPAN")
+        let txt = document.createTextNode("\u00D7")
+        span.className = "close"
+        span.appendChild(txt)
+        li.appendChild(span) 
+
+        // Научили новый крестик удалять таск при клике
+        span.onclick = function () {
+            let div = this.parentElement
+            div.style.display = "none"
+        }
+    }
+    document.getElementById("myInput1").value = ""
+} 
+
+
